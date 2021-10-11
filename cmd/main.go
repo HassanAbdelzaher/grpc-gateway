f945557f6b51d06ad15b03e77a20ec46c58b87d4
hassanabdelzaher <hasnmhmd77@gmail.com>
2021-10-11 16:41:26 +0200

    adding version

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"log"
+
 	"github.com/wmdev4/shipswift-gateway/core"
 )
 
+var VERSION = "1.1.1"
+
 func main() {
+	log.Println("staring gateway version " + VERSION)
 	core.Start()
 }
